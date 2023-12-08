644f37a66fcdd849429f1167f043d1caa5634437
Diego <diegoas1205@gmail.com>
2023-12-08 15:17:30 -0500

    [Diego] Base split text into array to allow syntax highlight

--- app/@editor/page.js
+++ app/@editor/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from "react"
+import { Fragment, useEffect, useRef, useState } from "react"
 
 export default function Editor() {
 
@@ -29,14 +29,35 @@ export default function Editor() {
     }
 
 
+    const concatenateReducer = (previous, current) => {
+        console.log({previous})
+        console.log({current})
+        if(current !== " " && current !== "\t" && current !== "\n"){
+            previous[previous.length-1] = previous[previous.length-1] + current
+        }
+            
+        else previous = [...previous, current, ""];
 
+        return previous;
+    }
 
     return (
         <div className="col-span-1">
             <textarea className="h-0 w-0 absolute" onChange={(e) => setCode(e.target.value)} value={code} ref={textRef} onKeyDown={KeyDownHandler}/>
             <div className="bg-[#2c2c2e] text-lg text-white p-2 font-mono h-full cursor-pointer whitespace-pre-wrap" onClick={() => textRef.current.focus()}>
                 {/* Create html like elements */}
-                {code}
+                {code.split("").reduce(concatenateReducer, [""]).map((val, index) => 
+                (
+                    <Fragment key={index+val} >
+                        {
+                            val !== " " && val !== "\t" && val !== "\n" ? 
+                            <span className="text-green-100">{val}</span> : <>{val}</>
+
+                        }
+                    
+                    </Fragment>
+                
+                ))}
             </div>
         </div>
         
